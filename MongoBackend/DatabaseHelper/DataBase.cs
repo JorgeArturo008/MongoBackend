500ce796be6f917a8b159e9af7ed550802833fd0
Jorge Arturo <Jorge Arturo@DESKTOP-4PAN6Q4>
2022-12-09 19:06:58 -0600

    Agregar archivos de proyecto.

---
+++ MongoBackend/DatabaseHelper/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using MongoBackend.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoBackend.DatabaseHelper
+{
+    public class DataBase
+    {
+        public List<Users> GetUsers()
+        {
+            MongoClient mongoClient = new MongoClient("mongodb+srv://Root:multicaribe1@cluster0.owjr3hy.mongodb.net/test");
+
+            IMongoDatabase db = mongoClient.GetDatabase("MongoBackend");
+
+            var users = db.GetCollection<BsonDocument>("Users");
+
+            List<BsonDocument> userArray = users.Find(new BsonDocument()).ToList();
+
+            List<Users> userList = new List<Users>();
+
+            foreach (BsonDocument bsonuser in userArray)
+            {
+                Users user = BsonSerializer.Deserialize<Users>(bsonuser);
+                 userList.Add(user);
+
+            }
+
+                return userList;
+        }
+
+    }
+}
---
+++ MongoBackend/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿namespace MongoBackend.Models
+{
+    public class Users
+    {
+
+        public int? _id { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public int? phone { get; set; }
+
+        public string adress { get; set; }
+
+        public DateTime dateIn { get; set; }
+    }
+}
